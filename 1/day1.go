36626d551e4bc00c577ad6e2ce9167e06e1871c6
Kyle Mistele <kyle@mistele.com>
2023-03-28 14:30:47 -0500

    day 1 part 2

--- 1/day1.go
+++ 1/day1.go
@@ -51,7 +51,49 @@ func main() {
 	// iterate over elf totals and print them
 	for idx, calorieCount := range elfCalorieTotals {
 		fmt.Printf("Elf #%d total: %d\n", idx+1, calorieCount)
-
 	}
 
+	// Find the elf with the most calories
+	var maxCalories int = 0
+	var maxCaloriesIdx int = 0
+	for idx, calorieCount := range elfCalorieTotals {
+		if calorieCount > maxCalories {
+			maxCalories = calorieCount
+			maxCaloriesIdx = idx
+		}
+	}
+
+	fmt.Printf("Elf #%d had the most calories with %d\n", maxCaloriesIdx+1, maxCalories)
+
+	// find the sum of the calories for the 3 elves with the most calories
+	var topThreeCalories [3]int
+	var topThreeCaloriesIdx [3]int
+	for idx, calorieCount := range elfCalorieTotals {
+		if calorieCount > topThreeCalories[0] {
+			topThreeCalories[2] = topThreeCalories[1]
+			topThreeCalories[1] = topThreeCalories[0]
+			topThreeCalories[0] = calorieCount
+			topThreeCaloriesIdx[2] = topThreeCaloriesIdx[1]
+			topThreeCaloriesIdx[1] = topThreeCaloriesIdx[0]
+			topThreeCaloriesIdx[0] = idx
+		} else if calorieCount > topThreeCalories[1] {
+			topThreeCalories[2] = topThreeCalories[1]
+			topThreeCalories[1] = calorieCount
+			topThreeCaloriesIdx[2] = topThreeCaloriesIdx[1]
+			topThreeCaloriesIdx[1] = idx
+		} else if calorieCount > topThreeCalories[2] {
+			topThreeCalories[2] = calorieCount
+			topThreeCaloriesIdx[2] = idx
+		}
+	}
+
+	// Find the sum of the calories of the top 3 elves with the most calories
+
+	// Print the top 3 elves
+	fmt.Println(topThreeCalories, topThreeCaloriesIdx)
+	var sumOfTopThree int = 0
+	for _, topCalorieAmount := range topThreeCalories {
+		sumOfTopThree += topCalorieAmount
+	}
+	fmt.Println("the sum of the top 3 calories is ", sumOfTopThree)
 }
